fbcbc1449d53af263d282cc25d6b45d086376306
siwar9910 <siwarayari1999@gmail.com>
2022-01-06 03:02:36 -0800

    task0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+	 new_matrix = []
+    c = 0
+    for i in matrix:
+        new = matrix[c].copy()
 for j in range(len(X[0])):
        result[i][j] = X[i][j] + Y[i][j]
 
