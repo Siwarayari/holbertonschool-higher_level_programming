4955681f3381210de13020746250e1d975558d47
siwar9910 <siwarayari1999@gmail.com>
2022-01-31 11:00:26 -0800

    task3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+is_kind_of_class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ function to compare object to instance """
+    return(isinstance(obj, a_class))
+© 2022 GitHub, Inc.
+Terms
+Privacy
